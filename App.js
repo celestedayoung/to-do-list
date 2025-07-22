af95f3db68fb0b21fd3a835fe101be8180f869c8
celestedayoung <dayoungpyo@gmail.com>
2025-07-22 23:23:57 +0900

    feat: 할 일 상태 표시 구현

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import TodoList from "./components/TodoList.js";
 import TodoInput from "./components/TodoInput.js";
+import TodoCounter from "./components/TodoCounter.js";
 import { StorageUtil } from "./utils/storage.js";
 
 function App() {
   this.data = [];
   this.todoList = null;
   this.todoInput = null;
+  this.todoCounter = null;
   this.$todoList = document.querySelector("#todo-list");
 
   this.init = () => {
@@ -17,6 +19,7 @@ function App() {
     this.data = newData;
     StorageUtil.saveTodos(this.data);
     this.todoList.update(this.data);
+    this.todoCounter.update(this.data);
   };
 
   this.addTodo = (newTodo) => {
@@ -41,14 +44,18 @@ function App() {
       <div class="app-container">
         <h1>Todo List</h1>
         <div id="todo-input"></div>
+        <div id="todo-counter"></div>
         <div id="todo-list-content"></div>
       </div>
     `;
 
     const todoInputContainer = document.querySelector("#todo-input");
+    const todoCounterContainer = document.querySelector("#todo-counter");
     const todoListContainer = document.querySelector("#todo-list-content");
 
     this.todoInput = new TodoInput(todoInputContainer, this.addTodo);
+
+    this.todoCounter = new TodoCounter(todoCounterContainer, this.data);
 
     this.todoList = new TodoList(
       todoListContainer,
